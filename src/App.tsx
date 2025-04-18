018d49d6a241e6e3e198e036cf0eac99604b6305
KANTNOLI <zahargysev02@gmail.com>
2025-04-19 00:30:14 +0300

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import Pages from "./component/Pages";
 
 function App() {
   return (
-    <Routes>
+    <>
       <Pages />
-      <Route path="/" element={<p>main page</p>} />
-      <Route path="/store/*" element={<Store />} />
-      <Route path="/game/:title" element={<p>asd</p>} />
+      <Routes>
+        <Route path="/" element={<p>main page</p>} />
+        <Route path="/store/*" element={<Store />} />
+        <Route path="/game/:title" element={<p>asd</p>} />
 
-      <Route path="*" element={<p>Error</p>} />
-    </Routes>
+        <Route path="*" element={<p>Error</p>} />
+      </Routes>
+    </>
   );
 }
 
--- src/component/Pages/index.tsx
+++ src/component/Pages/index.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router";
 import style from "./Pages.module.css";
 
 function Pages() {
-  return <section>asd</section>;
+  return (
+    <section className={style.body}>
+      <Link to="/">Home</Link>
+      <Link to="/store">Store</Link>
+    </section>
+  );
 }
 
 export default Pages;
