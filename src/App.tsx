fbf114c4f2ef98b96347a1f92c0878cac4f91fb5
KANTNOLI <zahargysev02@gmail.com>
2025-04-19 01:36:08 +0300

    store

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
 import { Route, Routes } from "react-router";
 import Store from "./component/Store";
 import Pages from "./component/Pages";
+import { useState } from "react";
+
+export interface Game {
+  title: string;
+  price: number;
+  downloads: number;
+}
+function rand(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const RenderListOriginal: Game[] = Array.from(
+  { length: 10 },
+  (): Game => ({
+    title: `Title rand(${rand(1, 9)},${rand(1, 9)},${rand(1, 9)})`,
+    price: rand(100, 1000),
+    downloads: rand(0, 100000),
+  })
+);
 
 function App() {
+  const [GamesList, setGamesList] = useState<Game[]>(RenderListOriginal);
+
   return (
     <>
       <Pages />
       <Routes>
         <Route path="/" element={<p>main page</p>} />
-        <Route path="/store/*" element={<Store />} />
+        <Route
+          path="/store/*"
+          element={
+            <Store
+              GamesList={GamesList}
+              setGamesList={(value: Game[]) => setGamesList(value)}
+            />
+          }
+        />
         <Route path="/game/:title" element={<p>asd</p>} />
 
         <Route path="*" element={<p>Error</p>} />
--- src/component/Store/index.tsx
+++ src/component/Store/index.tsx
@@ -1,7 +1,78 @@
 import style from "./Store.module.css";
 
-function Store() {
-  return <section>asd</section>;
+import { Game } from "../../App";
+import { useNavigate } from "react-router";
+
+interface props {
+  GamesList: Game[];
+  setGamesList: (value: Game[]) => void;
+}
+
+function rand(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function Store({ GamesList, setGamesList }: props) {
+  const navigate = useNavigate();
+  const btns: string[] = ["title", "price", "download"];
+
+  const filterStore = (type: number) => {
+    switch (type) {
+      case 0:
+        setGamesList([
+          ...GamesList.sort((a, b) => a.title.localeCompare(b.title)),
+        ]);
+        break;
+      case 1:
+        setGamesList([...GamesList.sort((a, b) => a.price - b.price)]);
+        break;
+      case 2:
+        setGamesList([...GamesList.sort((a, b) => a.downloads - b.downloads)]);
+        break;
+    }
+  };
+  const GamesPop = (): Game[] => {
+    const trash = [...GamesList];
+    trash.pop();
+    return trash;
+  };
+  const GamesPush = (): Game[] => {
+    const trash = [...GamesList];
+    trash.push({
+      title: `NEW rand(${rand(1, 9)},${rand(1, 9)},${rand(1, 9)})`,
+      price: rand(100, 1000),
+      downloads: 0,
+    });
+    return trash;
+  };
+
+  return (
+    <section className={style.body}>
+      <div className={style.filter}>
+        {btns.map((btn, id) => (
+          <button key={id} onClick={() => filterStore(id)}>
+            {btn}
+          </button>
+        ))}
+        <button onClick={() => setGamesList(GamesPop())}>pop</button>
+        <button onClick={() => setGamesList(GamesPush())}>add rand</button>
+      </div>
+      {GamesList.map((game, id) => (
+        <div
+          onClick={() =>
+            navigate(`/game/${id}?flag=${id % 2 ? "true" : "false"}`)
+          }
+          className={style.cart}
+          key={id}
+        >
+          <p>Title: {game.title}</p>
+          <p>Price: {game.price}</p>
+          <p>Downloads: {game.downloads}</p>
+          <p>flag: {id % 2 ? "true" : "false"}</p>
+        </div>
+      ))}
+    </section>
+  );
 }
 
 export default Store;
